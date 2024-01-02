60f765f5269cfb8db27cf78c87bf4477bca84751
Théophile Diot <tdiot@bunkerity.com>
2024-01-02 12:50:35 +0000

    Replace open-appsec with the attachment repo

--- src/deps/src/open-appsec/nodes/http_transaction_handler/package/exception.py
+++
@@ -1,8 +0,0 @@
-class SnortHookException(Exception):
-
-    def __init__(self, message="", practice_id=""):
-        self.message = message
-        self.practice_id = practice_id
-
-    def __str__(self):
-        return "{}".format(self.message)
--- src/deps/src/open-appsec/nodes/http_transaction_handler/package/snort3_to_ips/main.py
+++
@@ -1,7 +0,0 @@
-import snort3_to_ips.Signature.Signatures as Signatures
-
-
-def convert_incoming_rules(rules):
-    signatures = Signatures.Signatures()
-    signatures.load_snort_signatures(rules)
-    return signatures.get_payload_data()
